41c8b3a1250fdb8b77b9e6ab0947977f40da3ff9
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-08 11:47:02 +0300

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,6 +11,11 @@ class BaseModelTestCase(unittest.TestCase):
 	"""
 	Class for testing BaseModel
 	"""
+	def test_doc(self):
+		bm1 = BaseModel()
+		self.assertIsInstance(bm1.__doc__, str)
+		self.assertTrue(len(bm1.__doc__) > 0)
+
 	def test_id(self):
 		bm1 = BaseModel()
 		self.assertIsInstance(bm1.id, uuid.UUID)
