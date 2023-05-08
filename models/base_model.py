2a68ef899ec9513998fdc418afda0ce03e4e0592
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-08 11:49:43 +0300

    Changing indentation to spaces

--- models/base_model.py
+++ models/base_model.py
@@ -6,39 +6,39 @@ import uuid
 import datetime
 
 class BaseModel(object):
-	"""docstring for BaseModel"""
-	def __init__(self):
-		"""
-			The initializer for BaseModel
-			Creates: id, created_at and updated_at
-		"""
-		self.id = uuid.uuid4()
-		self.created_at = datetime.datetime.now()
-		self.updated_at = datetime.datetime.now()
+    """docstring for BaseModel"""
+    def __init__(self):
+        """
+            The initializer for BaseModel
+            Creates: id, created_at and updated_at
+        """
+        self.id = uuid.uuid4()
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
-	def __str__(self):
-		"""
-		Creates the String representation of BaseModel in
-		the form of:
-		[<class name>] (<self.id>) <self.__dict__>
-		"""
-		s = "[{2}] ({0}) {1}".format(self.id, self.__dict__,
-			self.__class__.__name__)
-		return s
+    def __str__(self):
+        """
+        Creates the String representation of BaseModel in
+        the form of:
+        [<class name>] (<self.id>) <self.__dict__>
+        """
+        s = "[{2}] ({0}) {1}".format(self.id, self.__dict__,
+            self.__class__.__name__)
+        return s
 
-	def save(self):
-		"""
-		Updates the updated_at to current time
-		"""
-		self.updated_at = datetime.datetime.now()
+    def save(self):
+        """
+        Updates the updated_at to current time
+        """
+        self.updated_at = datetime.datetime.now()
 
-	def to_dict(self):
-		"""
-		Returns a dictionary containing all keys/values
-		of __dict__ of the instance
-		"""
-		dict_rep = self.__dict__
-		dict_rep["__class__"] = self.__class__.__name__
-		dict_rep["updated_at"] = self.updated_at.isoformat()
-		dict_rep["created_at"] = self.created_at.isoformat()
-		return dict_rep
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values
+        of __dict__ of the instance
+        """
+        dict_rep = self.__dict__
+        dict_rep["__class__"] = self.__class__.__name__
+        dict_rep["updated_at"] = self.updated_at.isoformat()
+        dict_rep["created_at"] = self.created_at.isoformat()
+        return dict_rep
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -8,22 +8,22 @@ import uuid
 import datetime
 
 class BaseModelTestCase(unittest.TestCase):
-	"""
-	Class for testing BaseModel
-	"""
-	def test_doc(self):
-		bm1 = BaseModel()
-		self.assertIsInstance(bm1.__doc__, str)
-		self.assertTrue(len(bm1.__doc__) > 0)
+    """
+    Class for testing BaseModel
+    """
+    def test_doc(self):
+        bm1 = BaseModel()
+        self.assertIsInstance(bm1.__doc__, str)
+        self.assertTrue(len(bm1.__doc__) > 0)
 
-	def test_id(self):
-		bm1 = BaseModel()
-		self.assertIsInstance(bm1.id, uuid.UUID)
+    def test_id(self):
+        bm1 = BaseModel()
+        self.assertIsInstance(bm1.id, uuid.UUID)
 
-	def test_createdAt(self):
-		bm1 = BaseModel()
-		self.assertIsInstance(bm1.created_at, datetime.datetime)
+    def test_createdAt(self):
+        bm1 = BaseModel()
+        self.assertIsInstance(bm1.created_at, datetime.datetime)
 
-	def test_updateedAt(self):
-		bm1 = BaseModel()
-		self.assertIsInstance(bm1.updated_at, datetime.datetime)
+    def test_updateedAt(self):
+        bm1 = BaseModel()
+        self.assertIsInstance(bm1.updated_at, datetime.datetime)
