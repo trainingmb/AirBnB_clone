30e0af9f2e6eb7a5fead24e5fda189700bac14bd
trainingmb <118762148+trainingmb@users.noreply.github.com>
2023-05-11 02:21:22 +0300

    Pycodetest fix

--- models/user.py
+++ models/user.py
@@ -7,17 +7,23 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-	"""
-	doc string for the User Class
-	"""
-	def __init__(self, *args, **kwargs):
-		"""
-		"""
-		self.email = ""
-		self.password = ""
-		self.first_name = ""
-		self.last_name = ""
-		if (kwargs is not None) and (len(kwargs) > 0):
-			super(User, self).__init__(*args, **kwargs)
-		else:
-			super(User, self).__init__()
+    """
+    doc string for the User Class
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        Initilizer for User
+        Public class attributes:
+            email: string - empty string
+            password: string - empty string
+            first_name: string - empty string
+            last_name: string - empty string
+        """
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
+        if (kwargs is not None) and (len(kwargs) > 0):
+            super(User, self).__init__(*args, **kwargs)
+        else:
+            super(User, self).__init__()
